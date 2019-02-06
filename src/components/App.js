35a7560513b038237e37e8a68cb3718944bfa7ce
Duy Truong <duytruongh@gmail.com>
2019-02-06 12:01:23 -0800

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-	return <div>
-	Page One
-	<a href='/pagetwo'>Navigate to page two</a> 
-	</div>
-};
-
-const PageTwo = () => {
-	return <div>
-	Page Two 
-	<button>click me</button>
-	<a href='/'>Navigate to page one</a> 
-	</div>
-};
+import { Router, Route, Switch } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+import history from '../history';
 
 const App = () => {
 	return (
-		<div> 
-			<BrowserRouter>
+		<div className='ui container'> 
+			
+			<Router history={history}>
 				<div>
-				<Route path="/" exact component={PageOne} />
-				<Route path="/pageTwo" component={PageTwo} />
+					<Header />
+					<Switch>
+						<Route path='/' exact component={StreamList}/>
+						<Route path='/streams/new' exact component={StreamCreate}/>
+						<Route path='/streams/edit/:id' exact component={StreamEdit}/>
+						<Route path='/streams/delete/:id' exact component={StreamDelete}/>
+						<Route path='/streams/:id' exact component={StreamShow}/>
+					</Switch>
 				</div>
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 };
